64adfab10f8fd03f09dd5c356c6a6859e78f9a1d
Guth27 <guth27@gmail.com>
2018-07-25 12:52:18 +0200

    First Commit

---
+++ LearningApp/www/scripts/loginPage.js
@@ -0,0 +1,7 @@
+﻿(function () {
+    document.getElementById("myButton").addEventListener("click", loginClicked);
+
+    function loginClicked() {
+        window.location.href = "mainScreen.html";
+    }
+})();
