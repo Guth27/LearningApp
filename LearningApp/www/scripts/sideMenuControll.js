3bf7ee17bd456694bcdda55fca50ff989e9f450a
Guth27 <guth27@gmail.com>
2018-07-26 12:12:06 +0200

    SideMenu Test

---
+++ LearningApp/www/scripts/sideMenuControll.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    var menu = document.getElementById("sideMenu");
+    var button = document.getElementById("sideMenuButton");
+    button.addEventListener("click", menuButtonClicked);
+
+    function menuButtonClicked() {
+        console.log("click");
+        if (menu.style.left == "-200px") {
+            menu.style.left = "0px";
+        }
+        else {
+            menu.style.left = "-200px";
+        }
+    }
+})();
