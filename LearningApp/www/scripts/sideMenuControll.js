98b6210c27a388d043ea54959392261a8e0415fd
Guth27 <guth27@gmail.com>
2018-07-27 10:13:06 +0200

    SideMenuImprovement

--- LearningApp/www/scripts/sideMenuControll.js
+++ LearningApp/www/scripts/sideMenuControll.js
@@ -1,15 +1,19 @@
 ﻿(function () {
     var menu = document.getElementById("sideMenu");
-    var button = document.getElementById("sideMenuButton");
-    button.addEventListener("click", menuButtonClicked);
+    document.getElementById("sideMenuButton").addEventListener("click", menuButtonClicked);
+    document.getElementById("sideMenuCloseButton").addEventListener("click", menuButtonClicked);
+    document.getElementById("logOutButton").addEventListener("click", logOut);
 
     function menuButtonClicked() {
-        console.log("click");
-        if (menu.style.left == "-200px") {
+        console.log(menu.style.left);
+        if (menu.style.left == "-200px" || menu.style.left == "") {
             menu.style.left = "0px";
         }
         else {
             menu.style.left = "-200px";
         }
+    }
+    function logOut() {
+        window.location.href = "index.html";
     }
 })();
